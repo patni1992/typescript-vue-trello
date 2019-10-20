e66a5cae2bf08746451eb83d8656cb106a78f838
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-10-20 11:30:41 +0200

    init app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,23 @@
-const greeting: string = "hello worlzd";
+import * as express from 'express';
+import { PORT } from './constants';
+import { Routes } from './Routes';
+import * as Knex from 'knex';
+import { Model } from 'objection';
+import * as knexConfig from '../knexfile';
+import * as bodyParser from 'body-parser';
+const app = express();
 
-console.log(greeting)
+const dbENV = process.env.NODE_ENV || 'development';
+
+export const knex = Knex(knexConfig[dbENV]);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+Model.knex(knex);
+
+new Routes(app);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+export default app;
