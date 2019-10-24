ef903fbf2d18332e13716dc38b0b3136f82b3fb5
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-10-24 19:11:29 +0200

    setup errorHandler

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import * as Knex from 'knex';
 import { Model } from 'objection';
 import * as knexConfig from '../knexfile';
 import * as bodyParser from 'body-parser';
+import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 
 const dbENV = process.env.NODE_ENV || 'development';
@@ -19,5 +20,7 @@ Model.knex(knex);
 new Routes(app);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.use(errorHandler);
 
 export default app;
