8225a2ce4de0ff3280963ae2573b039ffb8bd11d
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-11-02 14:11:51 +0100

    add vue.js frontend, add scss and routes

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="app">
+        <router-view />
+    </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Lato:400,700|Roboto:400,700&display=swap');
+*,
+*::after,
+*::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+}
+
+h1 {
+    font-size: 3rem;
+    font-family: 'Roboto', sans-serif;
+}
+
+p,
+input,
+a {
+    font-family: 'Lato', sans-serif;
+}
+
+a {
+    color: var(--color-primary);
+}
+
+html {
+    // This defines what 1rem is
+    font-size: 62.5%; //1 rem = 10px; 10px/16px = 62.5%
+}
+
+body {
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.6rem;
+}
+</style>
