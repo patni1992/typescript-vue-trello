58e5ab6e90528a3a40b578b3c4fa53054f45009e
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-11-05 07:51:46 +0100

    add input component and test

---
+++ frontend/src/components/AppInput.vue
@@ -0,0 +1,51 @@
+<template>
+    <input
+        class="input"
+        :type="type"
+        :value="value"
+        :required="required"
+        v-on:input="updateValue($event.target.value)"
+    />
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+
+export default Vue.extend({
+    name: 'AppInput',
+    props: {
+        value: String,
+        type: {
+            type: String,
+            default: 'text',
+        },
+
+        required: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    methods: {
+        updateValue(value: string) {
+            this.$emit('input', value);
+        },
+    },
+});
+</script>
+
+<style scoped lang="scss">
+.input {
+    color: inherit;
+    font-family: inherit;
+    padding: 1.2rem 0 1rem 1.2rem;
+    border: 1.2px solid #cfcfcf;
+    outline: 0;
+    display: inline-block;
+    margin: 0 0 1rem 0;
+    padding-right: 1rem;
+    width: 100%;
+    color: black;
+    font-size: 1.6rem;
+    border-radius: 0.4rem;
+}
+</style>
---
+++ frontend/tests/unit/AppInput.spec.ts
@@ -0,0 +1,48 @@
+import { shallowMount, mount, Wrapper } from '@vue/test-utils';
+import AppInput from '@/components/AppInput.vue';
+
+let wrapper: Wrapper<any>;
+
+describe('AppButton', () => {
+    beforeEach(() => {
+        wrapper = shallowMount(AppInput);
+    });
+
+    test('is called', () => {
+        expect(wrapper.name()).toBe('AppInput');
+        expect(wrapper.isVueInstance()).toBeTruthy();
+    });
+
+    test('default type is text', () => {
+        expect(wrapper.find('input').attributes().type).toBe('text');
+    });
+
+    test('can set type password', () => {
+        wrapper = shallowMount(AppInput, {
+            propsData: {
+                type: 'password',
+            },
+        });
+        expect(wrapper.find('input').attributes().type).toBe('password');
+    });
+    test('render placeholder', () => {
+        wrapper = shallowMount(AppInput, {
+            propsData: {
+                placeholder: 'Nice input',
+            },
+        });
+        expect(wrapper.find('input').attributes().placeholder).toBe('Nice input');
+    });
+    
+    test('changing input value emits input event', () => {
+        wrapper = shallowMount(AppInput, {
+            propsData: {
+                placeholder: 'Nice input',
+            },
+        });
+
+        wrapper.find('input').setValue('hello 392439');
+
+        expect(wrapper.emitted().input[0]).toEqual(['hello 392439']);
+    });
+});
