3372d98f2a8991e1d859dd7b0c92b4efc835fd09
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-11-06 07:29:51 +0100

    Create Auth view, and boards view

---
+++ frontend/src/views/Auth.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <FullScreenImage>
+            <login-form />
+        </FullScreenImage>
+    </div>
+</template>
+
+<script>
+import FullScreenImage from '@/components/FullScreenImage.vue';
+import LoginForm from '@/components/LoginForm.vue';
+
+export default {
+    name: 'auth',
+    components: {
+        FullScreenImage,
+        LoginForm,
+    },
+};
+</script>
---
+++ frontend/src/views/Boards.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>This will be board page</h1>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: 'boards',
+};
+</script>
