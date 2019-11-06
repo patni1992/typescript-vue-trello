58c1288092c94d035a569b4ba217b7f6993bee05
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-11-06 07:52:52 +0100

    update routes

--- server/src/Routes.ts
+++ server/src/Routes.ts
@@ -1,10 +1,20 @@
 import { UsersController } from './controllers/users.controller';
+import { BoardsController } from './controllers/boards.controller';
 import * as express from 'express';
+import { auth } from './middlewares/auth';
 
 export class Routes {
     public usersController: UsersController = new UsersController();
+    public boardsController: BoardsController = new BoardsController();
 
     public constructor(app: express.Application) {
+        app.route('/login').post(this.usersController.login);
+        app.route('/register').post(auth, this.usersController.create);
+
+        app.route('/boards')
+            .get(auth, this.boardsController.index)
+            .post(auth, this.boardsController.create);
+
         app.route('/users')
             .get(this.usersController.index)
             .post(this.usersController.create);
