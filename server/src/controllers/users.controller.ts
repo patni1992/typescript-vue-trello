fe3aa7fa29a65ec443bd5e6b48019c6d944187da
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-11-06 07:52:40 +0100

    refactor user controller and login method

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import { User } from '../models/User';
-
+import * as jwt from 'jsonwebtoken';
+import { jwtKey } from '../../config.js';
 import { NextFunction } from 'connect';
+import { ExtendedRequest } from '../interfaces/ExtendedRequest';
 
 export class UsersController {
     public async index(req: Request, res: Response): Promise<Response> {
@@ -9,19 +11,31 @@ export class UsersController {
 
         return res.json(users);
     }
+
     public async create(req: Request, res: Response, next: NextFunction): Promise<any> {
-        try {
-            const { firstName, lastName, userName, email, password } = req.body;
-            const userExist = await User.exist(email, userName);
+        const { firstName, lastName, userName, email, password } = req.body;
+        const userExist = await User.getUser(email, userName);
 
-            if (userExist) {
-                return res.status(422).send({ message: 'Email or Username is already taken' });
-            }
-
-            const newUser = await User.query().insert({ firstName, lastName, userName, email, password });
-            return res.status(201).send(newUser);
-        } catch (error) {
-            return next(error);
+        if (userExist) {
+            return res.status(422).send({ message: 'Email or Username is already taken' });
         }
+
+        const newUser = await User.query().insert({ firstName, lastName, userName, email, password });
+        return res.status(201).send(newUser);
+    }
+
+    public async login(req: Request, res: Response, next: NextFunction): Promise<any> {
+        const { userName, password } = req.body;
+        const user = await User.getUser('', userName);
+
+        if (user) {
+            const isPasswordCorrect = await user.verifyPassword(password);
+
+            if (isPasswordCorrect) {
+                const token = jwt.sign({ id: user.id, email: user.email, userName: user.userName }, jwtKey);
+                return res.send(token);
+            }
+        }
+        return res.sendStatus(401);
     }
 }
