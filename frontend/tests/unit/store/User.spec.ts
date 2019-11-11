c72fe0fe38ac78c3af34571f5b7f4cfb45de5587
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-11-11 08:01:23 +0100

    add more test for user module mutations

--- frontend/tests/unit/store/User.spec.ts
+++ frontend/tests/unit/store/User.spec.ts
@@ -7,6 +7,31 @@ describe('User', () => {
         expect(user.email).toBe('testemail@.com');
     });
 
+    test('SET_USERNAME set username', () => {
+        user.SET_USERNAME('testUser');
+        expect(user.username).toBe('testUser');
+    });
+
+    test('SET_LAST_NAME set lastName', () => {
+        user.SET_LAST_NAME('someLastName');
+        expect(user.lastName).toBe('someLastName');
+    });
+
+    test('SET_FIRST_NAME set firstName', () => {
+        user.SET_FIRST_NAME('someFirstName');
+        expect(user.firstName).toBe('someFirstName');
+    });
+
+    test('SET_PROFILE_IMAGE set profileImage', () => {
+        user.SET_PROFILE_IMAGE('img.jpg');
+        expect(user.profileImage).toBe('img.jpg');
+    });
+
+    test('SET_TOKEN set token', () => {
+        user.SET_TOKEN('token_123456');
+        expect(user.token).toBe('token_123456');
+    });
+
     test('Login calls api and set data for token & user', async () => {
         const promise = user.login({
             username: 'john_doe',
