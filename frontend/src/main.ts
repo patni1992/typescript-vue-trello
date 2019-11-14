9f49f853d01d50748cee8dd759c9423664ea2efc
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-11-14 18:26:34 +0100

    add vClickOutside

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,9 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import './styles/main.scss';
+//@ts-ignore
+import vClickOutside from 'v-click-outside';
+Vue.use(vClickOutside);
 
 Vue.config.productionTip = false;
 
