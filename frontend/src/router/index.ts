4523a6bcd436e1bd87d904c9c5d9f124cf39f469
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-11-14 18:26:13 +0100

    addx auth guard

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Auth from '../views/Auth.vue';
 import Boards from '@/views/Boards.vue';
+import user from '@/store/user';
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,7 @@ const routes = [
         path: '/',
         name: 'auth',
         component: Auth,
+        meta: { public: true },
     },
     {
         path: '/about',
@@ -30,6 +32,20 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(route => !route.meta.public);
+
+    if (!user.isLoggedIn && requiresAuth) {
+        next({ name: 'auth' });
+    }
+
+    if (user.isLoggedIn && to.name === 'auth') {
+        next({ name: 'boards' });
+    }
+
+    next();
 });
 
 export default router;
