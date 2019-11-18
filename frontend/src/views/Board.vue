17bd9a2e64582a9977464db1fc65569903091a09
Patrik <patriknilsson1992@gmail.com>
2019-11-18 21:24:24 +0700

    add board view

---
+++ frontend/src/views/Board.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>This is id {{ $route.params.id }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import board, { BoardsData } from '@/store/board';
+import { Component, Vue } from 'vue-property-decorator';
+import CreateNewBoardCard from '@/components/CreateNewBoardCard.vue';
+
+@Component({
+    name: 'board',
+})
+export default class Boards extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
