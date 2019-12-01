47c4c6e0e09d213d38fe6099f963947d5365be1c
Patrik <patriknilsson1992@gmail.com>
2019-12-01 13:23:44 +0700

    add Markdown component

---
+++ frontend/src/components/MarkdownRender.vue
@@ -0,0 +1,28 @@
+<template>
+    <vue-markdown class="markdown-render">
+        <slot />
+    </vue-markdown>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+import VueMarkdown from 'vue-markdown';
+@Component({
+    name: 'markdownRender',
+    components: {
+        VueMarkdown,
+    },
+})
+export default class Card extends Vue {}
+</script>
+
+
+<style lang="scss">
+// Dont apply scoped, will not affect vue-markdown
+.markdown-render {
+    code {
+        white-space: normal;
+    }
+}
+</style>
