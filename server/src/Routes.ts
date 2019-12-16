76e8fe160a9e1a37f7618d07e89aeff8a687cd89
Patrik <patriknilsson1992@gmail.com>
2019-12-16 19:12:12 +0100

    clean up endpoints

--- server/src/Routes.ts
+++ server/src/Routes.ts
@@ -17,37 +17,41 @@ export class Routes {
     public boardColumnsController: BoardColumnsController = new BoardColumnsController();
 
     public constructor(app: express.Application) {
-        app.route('/login').post(this.usersController.login);
-        app.route('/register').post(this.usersController.create);
+        const router = express.Router();
 
-        app.route('/boards/:id')
+        router.route('/login').post(this.usersController.login);
+        router.route('/register').post(this.usersController.create);
+
+        router
+            .route('/boards/:id')
             .get(auth, this.boardsController.show)
             .put(auth, this.boardsController.update)
             .delete(auth, this.boardsController.delete);
 
-        app.route('/boards')
+        router
+            .route('/boards')
             .get(auth, this.boardsController.index)
             .post(auth, this.boardsController.create);
 
-        app.route('/boards/:boardId/columns').post(auth, this.boardColumnsController.create);
+        router.route('/boards/:boardId/columns').post(auth, this.boardColumnsController.create);
 
-        app.route('/columns/:columnId/cards').post(auth, this.columnCardsController.create);
+        router.route('/columns/:columnId/cards').post(auth, this.columnCardsController.create);
 
-        app.route('/columns')
+        router
+            .route('/columns')
             .get(auth, this.columnsController.index)
             .post(auth, this.columnsController.create);
 
-        app.route('/columns/:id')
+        router
+            .route('/columns/:id')
             .put(auth, this.columnsController.update)
             .delete(auth, this.columnsController.delete);
 
-        app.route('/columns/reorder').patch(auth, this.columnsController.reOrder);
+        router.route('/columns/reorder').patch(auth, this.columnsController.reOrder);
 
-        app.route('/cards/reorder').patch(auth, this.CardsController.reOrder);
-        app.route('/cards/:id').put(auth, this.CardsController.update);
+        router.route('/cards/reorder').patch(auth, this.CardsController.reOrder);
+        router.route('/cards/:id').put(auth, this.CardsController.update);
 
-        app.route('/users')
-            .get(this.usersController.index)
-            .post(this.usersController.create);
+        app.use('/', router);
     }
 }
